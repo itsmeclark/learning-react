1f27ff36db03a43e9a8b8a85c27452ce79f92661
itsmeclark <clarklayos123@gmail.com>
2025-06-10 01:24:27 +0800

    new function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import React, {useState} from "react"
+
 
 function App() {
 
+  const [tasks, setTask] = useState([]);
+  const [newTask, setNewTask] = useState('')
+
+  function handleInputChange(event){
+
+  }
+
+  function addTask(){
+
+  }
+
+  function deleteTask(index){
+
+  }
+
+  function moveTaskUp(index){
+
+  }
+  function moveTaskDown(index){
+    
+  }
+
   return (
-    <div></div>
+    <>
+
+    </>
   )
 }
 
