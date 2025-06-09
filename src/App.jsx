c1341ae1b7eea56ce90a031adb9e48c6d744c4ee
itsmeclark <clarklayos123@gmail.com>
2025-06-10 01:55:19 +0800

    new coded

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import React, {useState} from "react"
 
 function App() {
 
-  const [tasks, setTask] = useState([]);
+  const [tasks, setTask] = useState(["eat", "shower", 'walk dog']);
   const [newTask, setNewTask] = useState('')
 
   function handleInputChange(event){
-
+    setNewTask(event.target.value)
   }
 
   function addTask(){
-
+    setTask(t => [...t, newTask])
+    setNewTask("")
   }
 
   function deleteTask(index){
@@ -22,12 +23,34 @@ function App() {
 
   }
   function moveTaskDown(index){
-    
+
   }
 
   return (
     <>
+        <div className="to-do-list">
 
+          <h1>TO DO LIST</h1>
+
+          <div>
+            <input type="text" name="" id="" placeholder="Enter a task" value={newTask} onChange={handleInputChange}/>
+            <button className="add-button" onClick={addTask}>Add</button>
+          </div>
+
+          <ol>
+            {tasks.map((element, index)=>
+              <li key={index}>
+                <span className="text">{element}</span>
+
+                <button className="delete-button" onClick={()=>deleteTask(index)}>Delete</button>
+
+                <button className="move-button" onClick={()=>moveTaskUp(index)}>👆🏻</button>
+
+                <button className="move-button" onClick={()=>moveTaskDown(index)}>👇🏻</button>
+              </li>
+            )}
+          </ol>
+        </div>
     </>
   )
 }
